aaa215af4e6a6b3c51c2255a257e6f412abe444d
isra <you@example.com>
2023-02-17 11:11:06 +0100

    ?

--- js/isra.js
+++ js/isra.js
@@ -4,16 +4,55 @@
   .then(res => res.json()) // el método .json() analiza la respuesta JSON en un objeto literal JS
   .then(data => console.log(data));
 
+  // function filterProducts(categoria, stock, precio) {
+  //   return products.filter((product) => {
+  //     let meetscategoria = true;
+  //     let meetsStock = true;
+  //     let meetsprecio = true;
+  
+  //     if (categoria && product.categoria !== categoria) {
+  //       meetscategoria = false;
+  //     }
+  
+  //     if (stock && product.stock < stock) {
+  //       meetsStock = false;
+  //     }
+  
+  //     if (precio) {
+  //       for (const key in precio) {
+  //         if (product.precios[key] !== precio[key]) {
+  //           meetsprecio = false;
+  //           break;
+  //         }
+  //       }
+  //     }
+  
+  //     return meetscategoria && meetsStock && meetsprecio;
+  //   });
+  // }
+  
 
 
-
-
-  function filtrado(producto,tipoFiltro,valorFiltro)
-  {
-    productosF=[];
-
-    for(producto in data)
-    {
-        if(tipoFiltro==)
-    }
+  function OrdenarProductos(column) {
+    const productTable = document.getElementById("tabla-productos");
+    const rows = Array.from(productTable.getElementsByTagName("tr"));
+    const headerRow = rows.shift();
+    const sortedRows = rows.sort((a, b) => {
+      const aValue = a.getElementsByTagName("td")[column].innerText;
+      const bValue = b.getElementsByTagName("td")[column].innerText;
+  
+      if (column === 2 || column === 4) {
+        // Parsear los precios y números de ventas para que se ordenen correctamente
+        return parseFloat(aValue.slice(1)) - parseFloat(bValue.slice(1));
+      } else {
+        // Ordenar las cadenas de texto alfabéticamente
+        return aValue.localeCompare(bValue);
+      }
+    });
+  
+    // Volver a insertar las filas ordenadas en la tabla
+    productTable.innerHTML = "";
+    productTable.appendChild(headerRow);
+    sortedRows.forEach((row) => productTable.appendChild(row));
   }
+  

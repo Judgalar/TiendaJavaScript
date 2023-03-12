3a06ad41eb394480500fb32826eed09c762dbe3a
isra66 <95293138+isra66@users.noreply.github.com>
2023-03-12 12:18:38 +0100

    Add files via upload

---
+++ TiendaJavaScript-main/js/agregarCarrito.js
@@ -0,0 +1,21 @@
+function agregarCarrito(nombre,precio){
+  // Obtener la información del producto que se desea agregar al carrito
+  const nombreJuego = nombre;
+  const precioProducto = precio;
+
+  // Obtener los productos existentes en el carrito, si los hay
+  let carritoProductos = JSON.parse(localStorage.getItem("carritoProductos")) || {};
+
+  // Si el producto ya está en el carrito, aumentar su cantidad
+  if (carritoProductos[nombreJuego]) {
+    carritoProductos[nombreJuego].cantidad += 1;
+  } else {
+    // Si no está en el carrito, agregarlo con cantidad 1
+    carritoProductos[nombreJuego] = { precio: precioProducto, cantidad: 1 };
+  }
+
+  // Guardar el carrito actualizado en el localStorage
+  localStorage.setItem("carritoProductos", JSON.stringify(carritoProductos));
+
+  alert('Añadido a la cesta');
+}
---
+++ TiendaJavaScript-main/js/index.js
@@ -0,0 +1,11 @@
+
+import {cartasProductos, cartasProductosPromocion} from "./isra.js";
+
+//FETCH DEL INDEX
+fetch("productos.json")
+.then(res => res.json())
+.then((productos) => {
+cartasProductos('tendenciasItems', productos);
+cartasProductosPromocion('promocion', productos);
+});
+
